b30669468d7aad7b28cea62781388218a4e45b81
ToluFash <faithbullock4@gmail.com>
2021-08-25 12:55:12 +0100

    Changed from collateral to staff portal

--- ammilweb/views/index.js
+++ ammilweb/views/index.js
@@ -10779,7 +10779,7 @@ function dpi(){
 
                 ],this,180,77);
             let contactUs = new NavLink("contactUs","Contact","/contactus",this, 70);
-            this.collaterals = new NavLink("collaterals","Collaterals","http://collaterals.ammilmfi.com",this, 70);
+            this.collaterals = new NavLink("collaterals","Staff Portal","http://collaterals.ammilmfi.com",this, 70);
             this.servicesN.addComponent(this.servicesDD);
             this.resourceCnt.addComponent(this.resourceCntDD);
 
